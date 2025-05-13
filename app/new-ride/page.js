bfb69d02600bf270ae4b753f0e44ed9f14075776
Lyle Oliver <oliverlyle29@gmail.com>
2025-05-13 08:00:01 +0200

    callback

--- app/new-ride/page.js
+++ app/new-ride/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from "next/navigation";
 import { vehicleTypes } from "@/lib/pricing";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
